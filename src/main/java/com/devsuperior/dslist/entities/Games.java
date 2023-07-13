ff58f323a15ae9a03df315b89973ff2d883386ca
davi <eidavibalbino@gmail.com>
2023-07-13 16:39:21 -0300

    configurar ambiente

---
+++ src/main/java/com/devsuperior/dslist/entities/Games.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_game")
+public class Games {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platform;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public Games() {
+    }
+
+    public Games(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String longDescription) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platform = platform;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Games games = (Games) o;
+        return Objects.equals(id, games.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
